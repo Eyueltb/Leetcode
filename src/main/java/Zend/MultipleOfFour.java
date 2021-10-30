c63f30038487e07cf8d4e603ab3e630e7c91834e
Eyuel <eyueltb@gmail.com>
2021-10-30 08:11:58 +0200

    multiple of four using stream

--- src/main/java/Zend/MultipleOfFour.java
+++ src/main/java/Zend/MultipleOfFour.java
@@ -4,8 +4,7 @@ import java.util.stream.IntStream;
 
 /**
  * Write a function that prints every multiple of 4 between 1 and 333. Each number should be printed on a separate line in
- * descending order (the highest number on the first line). It doesn't matter if you print to the console, to an aspx-page or
- * to a WPF textbox.
+ * descending order (the highest number on the first line).
  */
 public class MultipleOfFour {
     private static void PrintMultipleOfFour() {
@@ -14,7 +13,7 @@ public class MultipleOfFour {
            System.out.println(i);
 
    }
-    private static void PrintMultipleOfFourStream(int from, int to) {
+    private static void PrintMultipleOfFourStream(int from, int to) { //map(i->to-i+from)->to reverse
         IntStream.rangeClosed(from,to).map(i->to-i+from).filter(i->i % 4 == 0).forEach(System.out::println);
     }
     public static void main(String[] args) {
